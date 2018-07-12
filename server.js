28bdcfa8d1f91102745db56f0b59fb60612e55c3
tuck_chutikarn <chutikarn@central.tech>
2018-07-12 17:33:07 +0700

    จัดการ Route แบบ GET request

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ app.get('/',(req,res) => {
     res.send('Root Page')
 })
 
+app.get('/member',(req,res) => {
+    res.send('Member Page')
+})
+
+app.get('/order',(req,res) => {
+    res.send('Order Page')
+})
+
+app.get('/order/:orderId',(req,res) => {
+    res.send('Order Page' + req.params.orderId)
+})
+
 app.listen(8000,() => {
     console.log('ready on http://localhost:8000')
 })
